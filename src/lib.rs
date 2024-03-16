c41ecbab93950ebb1deec6a4789667ce909b9655
sinYa Iwasaki <iwasaki@sinproject.net>
2024-03-16 16:03:04 +0900

    Error in Surface::configure: Validation Error #17

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,14 @@
 #![cfg_attr(coverage_nightly, feature(coverage_attribute))]
 
+mod wasm;
+
 use bevy::{asset::AssetMetaCheck, prelude::*};
 
 #[cfg_attr(coverage_nightly, coverage(off))]
 pub fn run() {
     App::new()
         .insert_resource(AssetMetaCheck::Never)
-        .add_plugins(DefaultPlugins)
+        .add_plugins(wasm::get_plugins())
         .add_systems(Startup, setup)
         .add_systems(Update, update_time)
         // .add_systems(Update, animate_transform)
